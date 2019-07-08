3cf186f57b1b4a0d750b280dfdb8cfadd7706698
Tanveer Ahmed <t.b.ahmed@accenture.com>
2019-07-08 15:09:58 +0530

    test code updated as per new requirement

--- test/inframod.Tests.ps1
+++ test/inframod.Tests.ps1
@@ -1,9 +1,31 @@
 Import-Module .\functionModule.psm1
-Describe 'function test 2 demo'{
-    It -Name 'Checks 5'{
-     Mock -CommandName f2 -MockWith {"admin"}
-        f1 | should be 4
-        Assert-VerifiableMock -Verbose
-    }
 
+
+#region Arrange block
+
+
+#endregion
+
+#region action block
+Describe 'function test 2 dev'{
+
+    context "mock scoped tests here"{
+
+    It -Name 'Vcenter check'{
+     Mock -CommandName f2 -MockWith {"admin"}
+        f1 | should contain "Prod"
+    }
+    It -Name "checks array value"{
+      $dir = f1
+      $dir[0] |should not be "Dev"
+      #$dir[0] | should not be "test1"
+
+    }
 }
+
+    Context "new mocked scope"{
+
+    }
+}
+
+#endregion
