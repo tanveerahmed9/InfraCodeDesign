2da16a5bca417956d7c903df00f1d4ee15dc258a
Tanveer Ahmed <tanveerahmed9@gmail.com>
2020-08-06 10:09:32 +0200

    stated devops pipeline demo

---
+++ azdvpspipia.ps1
@@ -0,0 +1,35 @@
+#region Scenario:
+<#you are a devops pro and you want tooling for devs to deploy their own web servers in Azure , its your job to
+build an azure devops pipeline that to support development of this tool using terraform to buid an Azure VM , configure the VM
+with DSC and run pester tests to isure that the VM was built successfully
+#>
+#endregion
+
+
+
+#region Important links to note to build these things yourself:-
+
+$psversiontable
+
+#endregion
+
+#region Assumption and prerequisite
+<#
+1. Install terraform on your computer - Done
+2. using PowerShell core -
+3. clone Infrastructure automator in your VScode
+4. PAT generated in your github repo
+5. Azure PowerShell module installed
+6. Azure CLI installed
+#>
+
+#endregion
+
+#region How this prject will flow
+
+#endregion
+
+#build initial infrastructure from scratch (one time thing)
+
+#endregion
+
